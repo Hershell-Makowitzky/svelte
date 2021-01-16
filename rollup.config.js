5ac44e3a59f2577150a2844f27fa4c634b233d9c
Hershell A. Makowitzky <hershell.makowitzky@pn.me>
2021-01-16 09:40:34 +0100

    untrack public folder

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import nodeResolve  from '@rollup/plugin-node-resolve';
 
 export default {
     input: "src/main.js",
-    output : {
+    output: {
         format: 'iife',
         file: './public/bundle.js',
         name: 'Test'
@@ -12,4 +12,10 @@ export default {
         svelte(),
         nodeResolve(),
     ],
+    input: "src/index.js",
+    output: {
+        format: 'html',
+        file: './public/index.html',
+        name: 'Test'
+    }
 }
